c262d7a998c6723c9a4f4a22fac8348dee353e2b
baddestnoder <132562739+baddestnoder@users.noreply.github.com>
2023-06-09 15:37:19 +0100

    Update atmCardSchema.js

--- route/atmCardSchema.js
+++ route/atmCardSchema.js
@@ -77,6 +77,6 @@ const thisSchema = new mongoose.Schema({
 
 
 
-const ThisModel = new mongoose.model("newatmagain", thisSchema);
+const ThisModel = new mongoose.model("anotherAtm", thisSchema);
 
 module.exports = ThisModel;
