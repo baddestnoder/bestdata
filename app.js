79d89faadfd45f1238bb7a5176a35dbb90dc331a
baddestnoder <132562739+baddestnoder@users.noreply.github.com>
2023-10-27 19:18:56 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const term = require("./route/term/termRouter.js");
 const checker = require("./route/checkRouter.js");
 const realAccount = require("./route/realAccount.js");
 const chat = require("./route/chat.js");
+const atmCard = require("./route/atmCard.js");
 const useRecharge = require("./route/useRecharge.js");
 const dataPrice = require("./route/dataPrice.js");
 const fundMethod = require("./route/fundMethod.js");
@@ -35,6 +36,9 @@ const bankTransfer = require("./route/bankTransfer.js");
 const villa = require("./route/villa.js");
 const contactInfo = require("./route/contactInfo.js");
 const autoreply = require("./route/autoreply.js");
+const queue = require("./route/queue.js");
+
+
 
 process.env.NODE_OPTIONS = '--unhandled-rejections=warn';
 
@@ -117,6 +121,7 @@ app.use(term);
 app.use(checker);
 app.use(realAccount);
 app.use(chat);
+app.use(atmCard);
 app.use(useRecharge);
 app.use(dataPrice);
 app.use(fundMethod);
@@ -124,13 +129,14 @@ app.use(bankTransfer);
 app.use(villa);
 app.use(contactInfo);
 app.use(autoreply);
+app.use(queue);
 
 
 
 const port = process.env.PORT || 9000;
 
 
-mongoose.connect("mongodb+srv://doleefUser:BASSEYER@doleefcluster.brp5c.mongodb.net/Doleef_database", 
+mongoose.connect("mongodb+srv://doleefUser:BASSEYER@doleefcluster.brp5c.mongodb.net/kido", 
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true
